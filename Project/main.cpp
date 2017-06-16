3dcecdfe7f41785135f99b4557efc93039aa9055
batbodyslam <vitsarutmwits20@gmail.com>
2017-06-16 13:24:52 +0700

    kinect recogn

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,25 +1,17 @@
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
 #include <iostream>
-using namespace cv;
-using namespace std;
-int main(int argc, char** argv)
+#include <sstream>
+
+#include "app.h"
+
+int main(int argc, char* argv[])
 {
-	if (argc != 2)
-	{
-		cout << " Usage: display_image ImageToLoadAndDisplay" << endl;
-		return -1;
+	try {
+		Kinect kinect;
+		kinect.run();
 	}
-	Mat image;
-	image = imread(argv[1], IMREAD_COLOR); // Read the file
-	if (image.empty()) // Check for invalid input
-	{
-		cout << "Could not open or find the image" << std::endl;
-		return -1;
+	catch (std::exception& ex) {
+		std::cout << ex.what() << std::endl;
 	}
-	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-	imshow("Display window", image); // Show our image inside it.
-	waitKey(0); // Wait for a keystroke in the window
+
 	return 0;
 }
